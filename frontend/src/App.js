34e41936724c2956483b3dedc4040fb3bc84c87e
DusanVelimirovic <dusan.velimirovic@yahoo.com>
2023-07-31 16:03:37 +0200

    Starting project, basic files structure, login page design

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+// Import external modules
+
+// Import internal modules
+import "./style.scss";
+import Login from "./pages/login/Login.jsx";
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/context/authContext.js
@@ -0,0 +1,38 @@
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+// Create and Export Auth Context API
+export const AuthContext = createContext();
+
+// Wrapp whole application
+// Use localStorage
+// Here we will store user object after authentication
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+
+  // handle user object after login
+  const login = async (inputs) => {
+    const res = await axios.post(
+      "http://localhost:8800/api/auth/login",
+      inputs,
+      {
+        withCredentials: true,
+      }
+    );
+
+    setCurrentUser(res.data);
+  };
+
+  // Pull user from localStorage
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
